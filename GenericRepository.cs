b053590ff20c9c347d9f5093c5ecd63409d4cce8
kidfox2001 <35247418+kidfox2001@users.noreply.github.com>
2018-10-05 16:58:06 +0700

    Add files via upload

--- GenericRepository.cs
+++ GenericRepository.cs
@@ -6,14 +6,20 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using TestService;
 
-namespace ServicePrs
+namespace TestService
 {
 
-    interface IGenericRepository<TEntity> where TEntity : class
+   public interface IGenericRepository<TEntity> where TEntity : class
     {
 
           IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+
+          IQueryable<TEntity> GetQueryable(
             Expression<Func<TEntity, bool>> filter = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             string includeProperties = "");
@@ -30,7 +36,7 @@ namespace ServicePrs
 
           void Delete(TEntity entityToDelete);
 
-          IEnumerable<TEntity> getByStoredProcedure(string spName, params SqlParameter[] parameters);
+          IEnumerable<TEntity> GetByStoredProcedure(string spName, params SqlParameter[] parameters);
 
     }
 
@@ -39,11 +45,17 @@ namespace ServicePrs
         internal DbContext context;
         internal DbSet<TEntity> dbSet;
 
+
         public GenericRepository(DbContext context)
         {
-            context.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+           
+
             this.context = context;
             this.dbSet = context.Set<TEntity>();
+
+            context.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+            //context.Database.Log = s => (new LogFile()).writeLog(s);
+            
         }
 
         public virtual IEnumerable<TEntity> Get(
@@ -71,6 +83,32 @@ namespace ServicePrs
             else
             {
                 return query.ToList();
+            }
+        }
+
+
+        public IQueryable<TEntity> GetQueryable(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query);
+            }
+            else
+            {
+                return query;
             }
         }
 
@@ -110,12 +148,117 @@ namespace ServicePrs
             context.Entry(entityToUpdate).State = EntityState.Modified;
         }
 
-        public virtual IEnumerable<TEntity> getByStoredProcedure(string spName, params SqlParameter[] parameters)
+        public virtual IEnumerable<TEntity> GetByStoredProcedure(string spName, params SqlParameter[] parameters)
         {
             string partPara = string.Join(",", parameters.Select(q => q.ParameterName).ToArray());
             string command = string.Format("exec {0} {1}", spName, partPara);
             return context.Database.SqlQuery<TEntity>(command, parameters).ToList();
         }
 
+    }
+
+
+    public class DummyGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        internal List<TEntity> items  = new List<TEntity>();
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>,
+            IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+
+            IQueryable<TEntity> query = from i in items.AsQueryable() select i ;
+
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+
+        }
+
+        public IQueryable<TEntity> GetQueryable(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = from i in items.AsQueryable() select i;
+
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query);
+            }
+            else
+            {
+                return query;
+            }
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return items;
+        }
+
+        public TEntity GetByID(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+             items.Add(entity);
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+           var item =  items.Where(i => i == entityToUpdate).SingleOrDefault();
+           if (item != null)
+           {
+               items.Remove(item);
+               items.Add(entityToUpdate);
+           }
+
+        }
+
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            items.Remove(entityToDelete);
+        }
+
+        public IEnumerable<TEntity> GetByStoredProcedure(string spName, params SqlParameter[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+
+     
     }
 }
---
+++ UnitOfWork.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestModel;
+using TestService;
+
+namespace TestService
+{
+
+    public interface IUnitOfWork : IDisposable
+    {
+
+        IGenericRepository<OPNCONTL> OPNCONTL_Repo { get; }
+
+        void Save();
+        void SaveWithLog();
+
+    }
+
+    public class UnitOfWorkDummy : IUnitOfWork
+    {
+
+        private DummyGenericRepository<OPNCONTL> _OPNCONTL;
+
+        public IGenericRepository<OPNCONTL> OPNCONTL_Repo
+        {
+            get
+            {
+                if (this._OPNCONTL == null)
+                {
+                    this._OPNCONTL = new DummyGenericRepository<OPNCONTL>();
+                }
+                return this._OPNCONTL;
+            }
+        }
+
+
+        public void Save()
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        public void SaveWithLog()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    //context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+
+    public class UnitOfWorkTest : IUnitOfWork
+    {
+
+        private ModelCsi context = new ModelCsi();
+
+        private GenericRepository<OPNCONTL> _OPNCONTL;
+        public IGenericRepository<OPNCONTL> OPNCONTL_Repo
+        {
+            get
+            {
+                if (this._OPNCONTL == null)
+                {
+                    this._OPNCONTL = new GenericRepository<OPNCONTL>(context);
+                }
+                return this._OPNCONTL;
+            }
+        }
+
+        private GenericRepository<TMP_TEST> _TMP_TEST;
+        public IGenericRepository<TMP_TEST> TMP_TEST
+        {
+            get
+            {
+                if (this._TMP_TEST == null)
+                {
+                    this._TMP_TEST = new GenericRepository<TMP_TEST>(context);
+                }
+                return this._TMP_TEST;
+            }
+        }
+
+        private GenericRepository<TEST_EF_MAIN> _TEST_EF_MAIN;
+        public IGenericRepository<TEST_EF_MAIN> TEST_EF_MAIN_Repo
+        {
+            get
+            {
+                if (this._TEST_EF_MAIN == null)
+                {
+                    this._TEST_EF_MAIN = new GenericRepository<TEST_EF_MAIN>(context);
+                }
+                return this._TEST_EF_MAIN;
+            }
+        }
+
+
+        private GenericRepository<TEST_EF_DESCRIPTION> _TEST_EF_DESCRIPTION;
+        public IGenericRepository<TEST_EF_DESCRIPTION> TEST_EF_DESCRIPTION_Repo
+        {
+            get
+            {
+                if (this._TEST_EF_DESCRIPTION == null)
+                {
+                    this._TEST_EF_DESCRIPTION = new GenericRepository<TEST_EF_DESCRIPTION>(context);
+                }
+                return this._TEST_EF_DESCRIPTION;
+            }
+        }
+
+        private GenericRepository<LOG_MAIN> _LOG_MAIN;
+        public IGenericRepository<LOG_MAIN> LOG_MAIN_Repo
+        {
+            get
+            {
+                if (this._LOG_MAIN == null)
+                {
+                    this._LOG_MAIN = new GenericRepository<LOG_MAIN>(context);
+                }
+                return this._LOG_MAIN;
+            }
+        }
+
+
+        private GenericRepository<LOG_TABLE> _LOG_TABLE;
+        public IGenericRepository<LOG_TABLE> LOG_TABLE_Repo
+        {
+            get
+            {
+                if (this._LOG_TABLE == null)
+                {
+                    this._LOG_TABLE = new GenericRepository<LOG_TABLE>(context);
+                }
+                return this._LOG_TABLE;
+            }
+        }
+
+        //public GenericRepository<T> getGeneric<T>() where T : class
+        //{
+        //    var repo = new GenericRepository<T>(context);
+        //    return repo;
+        //}
+
+
+        public void Save()
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    System.Diagnostics.Debug.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        System.Diagnostics.Debug.WriteLine("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                                            ve.PropertyName,
+                                            eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                                            ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+        }
+
+        public void SaveWithLog()
+        {
+            try
+            {
+                context.SaveChanges(true);
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    System.Diagnostics.Debug.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        System.Diagnostics.Debug.WriteLine("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                                            ve.PropertyName,
+                                            eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                                            ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
