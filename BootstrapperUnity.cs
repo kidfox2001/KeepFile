fcf6c75d22c5093121757f0c489a38bfa7c43c1d
kidfox2001 <35247418+kidfox2001@users.noreply.github.com>
2018-10-05 16:59:58 +0700

    Add files via upload

---
+++ BootstrapperUnity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+using Unity.Injection;
+using TestModel;
+using TestService;
+using TestService;
+
+namespace Testmvc
+{
+    public class BootstrapperUnity
+    {
+        public static IUnityContainer Initialise()
+        {
+            var container = BuildUnityContainer();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+            return container;
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+
+            // register all your components with the container here  
+            //This is the important line to edit  
+
+            container.RegisterType<IUnitOfWork, UnitOfWorkTest>();
+            container.RegisterType<TestManager>();
+            container.RegisterType<ILog, LogFile>();
+
+            RegisterTypes(container);
+            return container;
+        }
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
